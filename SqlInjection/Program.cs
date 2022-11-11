ae4783df05cb134b56641e6116967a949b51e4e1
chand <chand@DESKTOP-OG4TMVM>
2022-11-11 06:57:51 -0800

    Sample SQL injection

---
+++ SqlInjection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "ConnectionString";
+
+            string input = Console.ReadLine();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" + input + "' ORDER BY PRICE";
+                var adapter = new SqlDataAdapter(query1, connection);
+                var result = new DataSet();
+                adapter.Fill(result);
+            }   
+        }
+    }
+}
