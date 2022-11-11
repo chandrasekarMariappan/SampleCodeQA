9256d4b1059332069bcb66c03d56eebe5c848726
chand <chand@DESKTOP-OG4TMVM>
2022-11-11 05:14:35 -0800

    Scond commit

--- SampleCodeQA/Controllers/HomeController.cs
+++ SampleCodeQA/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,6 +20,16 @@ namespace SampleCodeQA.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+            var connc = @"DATA SOURCE=localhost:1521/xe;SELF TUNING=True;USER ID=system;password=p#11@STC;";
+            OracleConnection c = new OracleConnection(connc);
+            c.Open();
+            c.ClientInfo = "Sample Application";
+            c.ModuleName = "Chandrasekar";
+            c.ActionName = "Kafka";
+            OracleCommand cmd = new OracleCommand("select *from perf where id=3", c);
+            OracleDataAdapter adp = new OracleDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
         }
 
         public ActionResult Contact()
